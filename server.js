2728ae2068e81e7d1b7c06a710cc870c40befbf6
graslowsnail <graslowsnail@gmail.,com>
2022-10-24 14:08:14 -0500

    updated and added get request for animal by id

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { animals } = require('./data/animals.json');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -42,6 +43,10 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
@@ -49,6 +54,15 @@ app.get('/api/animals', (req, res) => {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 
